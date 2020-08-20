ee570b7dc5c8cb8d474ff121550713a74b62c4d2
Enspiron <enspirongaming@gmail.com>
2020-08-20 13:55:52 -0700

    unread adv stories

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ function CalculateFromTenfolds(TotalTenfolds) {
     return(TotalTenfolds*10)
 }
 
+function CalculateFromAdv(Adventurers) {
+    return(Adventurers*55)
+}
+
 function Calculate(e) {
     e.preventDefault();
-    let Total = (CalculateFromWyrmite(e.target.Wyrmite.value) + CalculateFromTenfolds(e.target.Tenfolds.value)) + parseInt(e.target.Singles.value)
+    let Total = (CalculateFromWyrmite(e.target.Wyrmite.value) + CalculateFromTenfolds(e.target.Tenfolds.value)) + parseInt(e.target.Singles.value) + Math.floor(parseInt(CalculateFromAdv(e.target.Stories.value)/120))
 
     console.dir(e.target.Wyrmite.value)
     TotalValue.innerText = Total;
